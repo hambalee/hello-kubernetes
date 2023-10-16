dcfefbf758227264e1f706f40b91b4b1619f01e4
hambalee <18182920+hambalee@users.noreply.github.com>
2023-10-16 22:57:40 +0700

    return string Hello Kubernetes

--- src/main/java/com/example/hellokubernetes/HelloKubernetesApplication.java
+++ src/main/java/com/example/hellokubernetes/HelloKubernetesApplication.java
@@ -2,12 +2,20 @@ package com.example.hellokubernetes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloKubernetesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloKubernetesApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(){
+		return String.format("Hello Kubernetes!");
 	}
 
 }
